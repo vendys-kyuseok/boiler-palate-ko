9455632fbc857c537807159f3f80ee3cc7c9a8d4
test-reacts <kyuseok@vendys.co.kr>
2020-11-12 13:48:25 +0900

    redux, reducer

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import LandingPage from './components/views/LandingPage/LandingPage'
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-const config = require('./Server/middleware/config/key');
+const config = require('./server/config/key');
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 const bodyParser = require('body-parser');
-const {User} = require('./models/User');
-const {auth} = require('./middleware/auth');
+const {User} = require('./server/models/User');
+const {auth} = require('./server/middleware/auth');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -22,6 +22,10 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => {
   res.send('Hello World! ha')
+})
+
+app.get('/api/hello', (req, res) => {
+  res.send('test test')
 })
  
 app.post('/api/users/register', (req, res) => {
